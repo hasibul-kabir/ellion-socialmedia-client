d42ed26edd14ee5dcd7eec9ca9c6142f5fed5131
reshat <reshat.kabir55@gmail.com>
2023-05-11 12:20:26 +0600

    login form handling|validation

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,21 +1,53 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { loginSchema } from '../../form-validations/loginValidation'
+import { useFormik } from 'formik'
 
 const LoginForm = () => {
+    const initialValues = {
+        email: '',
+        password: ''
+    }
+    const { values, errors, handleSubmit, handleChange, handleBlur, touched, resetForm } = useFormik({
+        initialValues: initialValues,
+        validationSchema: loginSchema,
+        onSubmit: (values, action) => {
+            console.log({ ...values });
+            action.resetForm();
+        }
+    })
     return (
         <div className="card-body">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Email</span>
                     </label>
-                    <input type="text" placeholder="email" className="input input-bordered" />
+                    <input
+                        type="text"
+                        placeholder="email"
+                        className="input input-bordered"
+                        id='email'
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                    {errors.email && touched.email && <p className='text-error text-xs py-1'>{errors.email}</p>}
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Password</span>
                     </label>
-                    <input type="password" placeholder="password" className="input input-bordered" />
+                    <input
+                        type="password"
+                        placeholder="password"
+                        className="input input-bordered"
+                        id='password'
+                        value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                    {errors.password && touched.password && <p className='text-error text-xs py-1'>{errors.password}</p>}
                     <label className="label">
                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                     </label>
